0821d5f31d674dab00f3655250f75f5c8e57a849
teach104 <teach104>
2016-07-06 12:06:15 +0300

    Initial commit

---
+++ app/src/main/java/tomerbu/edu/menusanddialogs/MainActivity.java
@@ -0,0 +1,83 @@
+package tomerbu.edu.menusanddialogs;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
+    RelativeLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        layout = (RelativeLayout) findViewById(R.id.layout);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagePickerDialog();
+            }
+        });
+
+    }
+
+    int[] images = {R.drawable.back1, R.drawable.back2, R.drawable.back3, R.drawable.back4, R.drawable.back5};
+    int currentIndex = 0;
+
+    private void showImagePickerDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("בחר תמונה").setPositiveButton(">", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                layout.setBackgroundResource(images[currentIndex]);
+                currentIndex++;
+                if (currentIndex>=images.length){currentIndex = 0;}
+            }
+        }).setNegativeButton("<", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                layout.setBackgroundResource(images[currentIndex]);
+                currentIndex--;
+                if (currentIndex < 0){currentIndex = images.length - 1;}
+            }
+        }).show();
+    }
+
+    //create the options menu from the menu_main.xml:
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //The On Click listener of the menu:
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_change_background) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
